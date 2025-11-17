a4f86cfd5e385c034a9fda08cae613d941d6f7d6
Oliver Pruett <ospruett19@gmail.com>
2025-11-17 14:19:25 -0500

    updated to handle mbpp dataset

--- helpers/text_handler.py
+++ helpers/text_handler.py
@@ -2,21 +2,31 @@ import json
 
 def jsonl_to_texts(filepath: str) -> list[str]:
     result = []
-    with open(filepath, 'r', encoding='utf-8') as f:
-        for line in f:
-            obj = json.loads(line)
-            line = ""
-            for key, val in obj.items():
-                match key:
-                    case 'desc':
-                        line += f"<|DESC|>{val}"
-                    case 'examples':
-                        line += f"<|EXAMPLES|>{val}"
-                    case 'constraints':
-                        line += f"<|CONSTRAINTS|>{val}"
-                    case 'solution':
-                        line += f"<|OUTPUT|>{val}"
-            result.append(line)
+    if filepath.endswith('.jsonl') is False:
+        raise ValueError("Filepath must point to a .jsonl file")
+    
+    if 'mbpp' in filepath.lower():
+        print("Detected MBPP dataset")
+        with open(filepath, 'r', encoding='utf-8') as f:
+            for line in f:
+                obj = json.loads(line)
+                text = f"<|DESC|>{obj['text']}\n<|EXAMPLES|>{"\n".join(obj['test_list'])}\n<|OUTPUT|>{obj['code']}"
+                result.append(text)
+    else:
+        print("Processing custom JSONL format")
+        with open(filepath, 'r', encoding='utf-8') as f:
+            for line in f:
+                obj = json.loads(line)
+                line = ""
+                for key, val in obj.items():
+                    match key:
+                        case 'desc':
+                            line += f"<|DESC|>{val}"
+                        case 'examples':
+                            line += f"<|EXAMPLES|>{val}"
+                        case 'constraints':
+                            line += f"<|CONSTRAINTS|>{val}"
+                        case 'solution':
+                            line += f"<|OUTPUT|>{val}"
+                result.append(line)
     return result
-
-# [print(text) for text in jsonl_to_texts('./corpuses/easy_array_python_0.6_8_1755876977.jsonl')]
