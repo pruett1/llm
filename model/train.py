17691f025931e0a9dfe924cf02b0e0a6732fc178
Oliver Pruett <ospruett19@gmail.com>
2025-11-21 15:50:49 -0500

    fixed collate and loss fn if no output token present (case of pretrain and ft 1)

--- model/train.py
+++ model/train.py
@@ -13,8 +13,14 @@ import os
 from tqdm import tqdm
 
 def masked_lm_loss(logits: torch.Tensor, labels: torch.Tensor, output_token_id: int, pad_id: int) -> torch.Tensor:
+    has_output = (labels == output_token_id).any(dim=1)
+
     mask = (labels == output_token_id).cumsum(dim=1) >= 1
     mask = mask & (labels != pad_id)
+
+    pad_only_mask = (labels != pad_id)
+
+    mask = torch.where(has_output[:, None], mask, pad_only_mask)
 
     logits = logits.reshape(-1, logits.size(-1))
     labels = labels.reshape(-1)
@@ -30,7 +36,12 @@ def collate_fn_factory(tokenizer: BlBPETokenizer, seq_len: int, total_epochs: in
     output_id = tokenizer.get_special_token_id("<|OUTPUT|>")
     desc_id = tokenizer.get_special_token_id("<|DESC|>")
     ex_id = tokenizer.get_special_token_id("<|EXAMPLES|>")
-    # cons_id = tokenizer.get_special_token_id("<|CONSTRAINTS|>")
+
+    def find_first(tokens, token_id):
+        idxs = (tokens == token_id).nonzero(as_tuple=True)
+        if len(idxs[0]) == 0:
+            return None
+        return idxs[0][0]
 
     # Smoothly increase context length over training epochs, always include desc gradually add examples+constraints
     # if context is too long, randomly select a subset that fits
@@ -45,40 +56,52 @@ def collate_fn_factory(tokenizer: BlBPETokenizer, seq_len: int, total_epochs: in
             t = torch.tensor(tokens, dtype=torch.long)
 
             # Get special token idxs
-            d_start = (t == desc_id).nonzero(as_tuple=True)[0].item()
-            e_start = (t == ex_id).nonzero(as_tuple=True)[0].item()
+            d_start = find_first(t, desc_id)
+            e_start = find_first(t, ex_id)
             # c_start = (t == cons_id).nonzero(as_tuple=True)[0]
-            o_start = (t == output_id).nonzero(as_tuple=True)[0].item()
+            o_start = find_first(t, output_id)
 
-            # Split into desc, examples+constraints, output
-            desc = t[d_start:e_start]
-            examples_constraints = t[e_start:o_start]
-            output = t[o_start:]
+            if d_start == None and e_start == None and o_start == None: #no special tokens -> pretraining or finetuning 1
+                if t.size(0) >= seq_len: #tokens are too long randomly select a subset
+                    start_idx = random.randint(0, t.size(0) - seq_len)
+                    t = t[start_idx:start_idx + seq_len]
+                elif t.size(0) < seq_len: #tokens are too short pad
+                    t = torch.cat([t, torch.full((seq_len-t.size(0),), pad_id, dtype=torch.long)], dim=0)
 
-            #if output is too long, randomly select a subset that fits
-            #if too long take 75% of seq_len from output (allows some context)
-            if output.size(0) >= seq_len:
-                sub_size = int(seq_len * 0.75)
-                start_idx = random.randint(0, output.size(0) - sub_size)
-                output = output[start_idx:start_idx + sub_size]
 
-            context = torch.cat([desc, examples_constraints], dim=0)
+            elif o_start == None and (d_start != None or e_start != None): #malformed data
+                continue
 
-            allowed_context_len = int((desc.size(0)) + (context.size(0) - desc.size(0)) * progress) # Smooth ramping of context
-            max_context_len = min(int(seq_len - output.size(0)), allowed_context_len) # Max size of context we can fit, capped to allowed_context_len
-
-            # If context is too long, randomly select a subset that fits
-            if allowed_context_len > max_context_len:
-                start_idx = random.randint(0, allowed_context_len - max_context_len)
-                context = context[start_idx:start_idx + max_context_len]
             else:
-                context = context[:allowed_context_len]
+                # Split into desc, examples+constraints, output
+                desc = t[d_start:e_start]
+                examples_constraints = t[e_start:o_start]
+                output = t[o_start:]
 
-            t = torch.cat([context, output], dim=0)
-            # Pad if needed
-            if t.size(0) < seq_len:
-                t = torch.cat([t, torch.full((seq_len - t.size(0),), pad_id, dtype=torch.long)], dim=0)
-            
+                #if output is too long, randomly select a subset that fits
+                #if too long take 75% of seq_len from output (allows some context)
+                if output.size(0) >= seq_len:
+                    sub_size = int(seq_len * 0.75)
+                    start_idx = random.randint(0, output.size(0) - sub_size)
+                    output = output[start_idx:start_idx + sub_size]
+
+                context = torch.cat([desc, examples_constraints], dim=0)
+
+                allowed_context_len = int((desc.size(0)) + (context.size(0) - desc.size(0)) * progress) # Smooth ramping of context
+                max_context_len = min(int(seq_len - output.size(0)), allowed_context_len) # Max size of context we can fit, capped to allowed_context_len
+
+                # If context is too long, randomly select a subset that fits
+                if allowed_context_len > max_context_len:
+                    start_idx = random.randint(0, allowed_context_len - max_context_len)
+                    context = context[start_idx:start_idx + max_context_len]
+                else:
+                    context = context[:allowed_context_len]
+
+                t = torch.cat([context, output], dim=0)
+                # Pad if needed
+                if t.size(0) < seq_len:
+                    t = torch.cat([t, torch.full((seq_len - t.size(0),), pad_id, dtype=torch.long)], dim=0)
+                
             inputs.append(t[:-1])
             labels.append(t[1:])
         
